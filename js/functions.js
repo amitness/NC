b4589a1e6bb0b62835764e409c6bc84af2cdf5f6
prabinzz <prabinparajuli92@gmail.com>
2017-07-28 20:58:20 +0545

    forex BaseValue mismatch fix

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,11 @@ function getData() {
   $.getJSON(localStorage.apiURL, function(data, status, xhr) {
     if (status == "success") {
       $.each(data.Conversion.Currency, function(key, val) {
-        localStorage[val.BaseCurrency]=val.TargetBuy;
+        if (val.BaseValue != "1") {
+          localStorage[val.BaseCurrency]=Number(val.TargetBuy)/Number(val.BaseValue);
+        }else{
+          localStorage[val.BaseCurrency]=Number(val.TargetBuy);
+        }
       });
       localStorage["lastUpdate"] = new Date();
       console.log("[*] data updated.");
