eade355583d446d75382187a1442f1f68f5ccb7d
prabinzz <prabinparajuli92@gmail.com>
2017-07-30 22:02:45 +0545

    filter and pausing system

--- js/background.js
+++ js/background.js
@@ -10,31 +10,52 @@ if (!localStorage.updateRate) {
 if (!localStorage.totalConverted) {
   localStorage.totalConverted = 0;
 }
-
+if (!localStorage.isPause) {
+  localStorage.isPause = false;
+}
 getData(); //get data from server
-setInterval(function () {
+setInterval(function() {
   getData(); // repeat every 10 min.
-}, 1000*60*10);
+}, 1000 * 60 * 10);
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   // serve data to content script
-    if (request.method == "getLocalStorage")
+  if (request.method == "getLocalStorage") {
     // {method : "getLocalStorage"} to get whole localStorage
-      if(request.key){
-        // {method: "getLocalStorage", key: "key"} to get single data
-        sendResponse({key : localStorage[request.key]});
-      }else{
-        sendResponse(localStorage); // send whole localStorage if key is not given
-      }
-    else{
-      sendResponse({}); // empty.
+    if (request.key) {
+      // {method: "getLocalStorage", key: "key"} to get single data
+      sendResponse({
+        key: localStorage[request.key]
+      });
+    } else {
+      sendResponse(localStorage); // send whole localStorage if key is not given
     }
+  }
 
-    if (request.method == "count") {
-        if (request.converted) {
-          // {method: "count", converted: num} to increase total converted value
-          localStorage["totalConverted"] = Number(localStorage["totalConverted"])
-            + Number(request.converted);
-        }
+  // ===========================================================================
+
+  if (request.method == "count") {
+    if (request.converted) {
+      // {method: "count", converted: num} to increase total converted value
+      localStorage["totalConverted"] = Number(localStorage["totalConverted"]) +
+        Number(request.converted);
     }
+  }
+
+  // ===========================================================================
+  if (request.method == "filter") {
+    // {method: 'filter', url: 'url'}
+    if (request.url) {
+      if (filter(request.url)) {
+        sendResponse({
+          status: true
+        });
+      } else {
+        sendResponse({
+          status: false
+        });
+      }
+    }
+  }
+  // ===========================================================================
 });
--- js/content.js
+++ js/content.js
@@ -2,47 +2,60 @@ chrome.runtime.sendMessage({
   method: "getLocalStorage"
 }, function(response) {
   var usd = Number(response.USD);
+  var localStorage = response;
   // ======================== TEST =============================
   regReplace(/(\$|usd|usd:)\s?(,?\.?\d)+/i, /[^0-9.]/ig, usd);
   setInterval(function() {
     regReplace(/(\$|usd|usd:)\s?(,?\.?\d)+/i, /[^0-9.]/ig, usd);
-  }, Number(response.updateRate));
+  }, Number(localStorage.updateRate) * 5);
 
 });
 
 
 function regReplace(reg, filterReg, rate) {
-  var total = 0;
   /*
     ARGS:
       reg:  RegEX to match currency string.
       filterReg:  RegEX to filter number from matched currency string.
       rate: exchange rate in NPR to conver.
    */
-  var elements = document.getElementsByTagName('*');
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+  chrome.runtime.sendMessage({
+      method: "filter",
+      url: location.href
+    },
+    function(response) {
+      if (response.status) {
+        var total = 0;
+        var elements = document.getElementsByTagName('*');
+        for (var i = 0; i < elements.length; i++) {
+          var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j];
+          for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-      if (node.nodeType === 3) {
-        var text = node.nodeValue;
-        var found = 0;
-        while (reg.test(text)) {
-          found++;
-          total++;
-          var cur_usd = reg.exec(text)[0].replace(filterReg, '');
-          text = text.replace(reg, "Rs. " + Math.ceil(rate * Number(cur_usd)));
+            if (node.nodeType === 3) {
+              var text = node.nodeValue;
+              var found = 0;
+              while (reg.test(text)) {
+                found++;
+                total++;
+                var cur_usd = reg.exec(text)[0].replace(filterReg, '');
+                text = text.replace(reg, "Rs. " + Math.ceil(rate * Number(cur_usd)));
+              }
+              if (found > 0) {
+                element.replaceChild(document.createTextNode(text), node);
+              }
+            }
+          }
         }
-        if (found > 0) {
-          element.replaceChild(document.createTextNode(text), node);
+        if (total > 0) {
+          console.log("[*] NC converted " + total + " price into NPR.");
+          chrome.runtime.sendMessage({
+            method: "count",
+            converted: total
+          });
         }
       }
-    }
-  }
-  if (total>0) {
-    console.log("[*] NC converted "+total+" price into NPR.");
-    chrome.runtime.sendMessage({method: "count", converted: total});
-  }
+    });
+
 }
